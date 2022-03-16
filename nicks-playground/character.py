68b7ef1e17fc6bbf015dc66c14537a0a19544d76
TheFinalTzar <44308833+TheFinalTzar@users.noreply.github.com>
2022-03-15 20:09:33 -0500

    Added speed
    implemented a speed value for characters, which responds to keypad +/- presses and has min/max

--- nicks-playground/character.py
+++ nicks-playground/character.py
@@ -3,12 +3,14 @@ import pygame
 
 class Character(pygame.sprite.Sprite):
 
-    def __init__(self, name="Default NPC", height=50, width=20):
+    def __init__(self, name="Default NPC", height=50, width=30, speed=1):
         super(Character, self).__init__()
         self.name = name
         self.surf = pygame.Surface((width, height))
         self.surf.fill((255, 255, 255))
         self.rect = self.surf.get_rect()
+        self.image = pygame.image.load("C:\\users\\nick\\pictures\\game_assets\\tiny_cage.png")
+        self.speed = speed
 
     def move(self, dx, dy, min_x=None, max_x=None, min_y=None, max_y=None):
         """ Moves the character's location with optional bounds """
@@ -25,11 +27,18 @@ class Character(pygame.sprite.Sprite):
                 self.rect.top = min_y
 
     def update(self, keys, screen_width, screen_height):
+        # Update speed first
+        if keys[pygame.K_KP_PLUS]:
+            self.speed = min (self.speed + 1, 10)
+        if keys[pygame.K_KP_MINUS]:
+            self.speed = max(self.speed - 1, 1)
+
+        # Update position
         if keys[pygame.K_w]:
-            self.move(0, -1, 0, screen_width, 0, screen_height)
+            self.move(0, -self.speed, 0, screen_width, 0, screen_height)
         if keys[pygame.K_a]:
-            self.move(-1, 0, 0, screen_width, 0, screen_height)
+            self.move(-self.speed, 0, 0, screen_width, 0, screen_height)
         if keys[pygame.K_s]:
-            self.move(0, 1, 0, screen_width, 0, screen_height)
+            self.move(0, self.speed, 0, screen_width, 0, screen_height)
         if keys[pygame.K_d]:
-            self.move(1, 0, 0, screen_width, 0, screen_height)
+            self.move(self.speed, 0, 0, screen_width, 0, screen_height)
--- nicks-playground/main.py
+++ nicks-playground/main.py
@@ -5,20 +5,7 @@
 import random
 
 import character
-
-
-def print_grid(w, h, char=None):
-    print("_" * (w + 2))
-    for i in range(h):
-        print("|", end='')
-        for j in range(w):
-            if char is not None and char.position.x - char.width <= j <= char.position.x + char.width \
-                    and char.position.y - char.height <= h-i-1 <= char.position.y + char.height:
-                print('X', end='')
-            else:
-                print('.', end='')
-        print("|")
-    print("-" * (w + 2))
+import pygame
 
 
 SCREEN_WIDTH = 1280
@@ -28,37 +15,12 @@ SCREEN_HEIGHT = 780
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    # print_grid(15, 10, basicNpc)
-    #
-    # basicNpc.move(2, -1, 15, 10)
-    # print_grid(15, 10, basicNpc)
-    #
-    # print("\n\n===== Bottom right =====")
-    # basicNpc.move(100, -100, 15, 10)
-    # print_grid(15, 10, basicNpc)
-    #
-    # print("\n\n===== Top right =====")
-    # basicNpc.move(100, 100, 15, 10)
-    # print_grid(15, 10, basicNpc)
-    #
-    # print("\n\n===== Bottom left =====")
-    # basicNpc.move(-100, -100, 15, 10)
-    # print_grid(15, 10, basicNpc)
-    #
-    # print("\n\n===== Top left =====")
-    # basicNpc.move(-100, 100, 15, 10)
-    # print_grid(15, 10, basicNpc)
-    # Simple pygame program
-
-    # Import and initialize the pygame library
-    import pygame
-
     pygame.init()
-
+    clock = pygame.time.Clock()
     # Set up the drawing window
     screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 
-    basicNpc = character.Character("Harold", 50, 20)
+    basicNpc = character.Character("Cage", 50, 30, 3)
 
     # Run until the user asks to quit
     running = True
@@ -74,13 +36,14 @@ if __name__ == '__main__':
 
         # Draw a solid blue circle in the center
         # pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
-        screen.blit(basicNpc.surf, basicNpc.rect)
+        screen.blit(basicNpc.image, basicNpc.rect)
         keys = pygame.key.get_pressed()
         basicNpc.update(keys, SCREEN_WIDTH, SCREEN_HEIGHT)
         # basicNpc.move(random.randint(0, 1), random.randint(0, 1), 0, SCREEN_WIDTH, 0, SCREEN_HEIGHT)
 
         # Flip the display
         pygame.display.flip()
+        clock.tick(50)
 
     # Done! Time to quit.
     pygame.quit()
