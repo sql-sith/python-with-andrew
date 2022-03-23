18ac03a962d02673c9d9ec3a31983c953f57d42e
TheFinalTzar <44308833+TheFinalTzar@users.noreply.github.com>
2022-03-22 20:47:04 -0500

    MOVING BACKGROUND
    the character now moves, and when approaching the edge of the background, the viewport adjusts accordingly

--- nicks-playground/character.py
+++ nicks-playground/character.py
@@ -6,19 +6,37 @@ MIN_SPEED = 1
 
 class Character(pygame.sprite.Sprite):
 
-    def __init__(self, name="Default NPC", height=50, width=30, speed=1):
+    def __init__(self, name="Default NPC", height=50, width=30, left_x: int = 0, top_y: int  = 0, speed=1):
+        """Basic Character constructor
+
+        :param name: Name of character (default: "Default NPC")
+        :param height: Height of character (default: 50)
+        :param width: Width of character (default: 30)
+        :param left_x: X of top left corner of character location (default: 0)
+        :param top_y: Y of top left corner of character location (default: 0)
+        :param speed: Speed of character (default: 1)
+        """
         super(Character, self).__init__()
         self.name = name
         self.surf = pygame.Surface((width, height))
-        self.surf.fill((255, 255, 255))
         self.rect = self.surf.get_rect()
+        self.rect.left = left_x
+        self.rect.top = top_y
         self.image = pygame.image.load("C:\\users\\nick\\pictures\\game_assets\\tiny_cage.png")
         self.speed = speed
 
-    def move(self, dx, dy, min_x=None, max_x=None, min_y=None, max_y=None):
-        """ Moves the character's location with optional bounds """
-        self.rect.move_ip(dx, 0)
-        self.rect.move_ip(0, dy)
+    def move(self, dx: int, dy: int, min_x: int = None, max_x: int = None, min_y: int = None, max_y: int = None):
+        """Moves the character.
+
+        :param dx: Distance (in pixels) to move in x direction
+        :param dy: Distance (in pixels) to move in y direction
+        :param min_x: (optional) Minimum character x value
+        :param max_x: (optional) Maximum character x value
+        :param min_y: (optional) Minimum character y value
+        :param max_y: (optional) Maximum character y value
+        :return: None
+        """
+        self.rect.move_ip(dx, dy)
         if min_x is not None and max_x is not None and min_y is not None and max_y is not None:
             if self.rect.left < min_x:
                 self.rect.left = min_x
@@ -29,7 +47,14 @@ class Character(pygame.sprite.Sprite):
             if self.rect.top < min_y:
                 self.rect.top = min_y
 
-    def update(self, keys, screen_width, screen_height):
+    def update(self, keys, screen_width: int, screen_height: int):
+        """Updates character based upon given key presses.
+
+        :param keys: List of keys pressed, given by PyGame engine
+        :param screen_width: Width of screen viewport
+        :param screen_height: Height of screen viewport
+        :return: None
+        """
         # Update speed first
         if keys[pygame.K_KP_PLUS]:
             self.speed = min(self.speed + 1, MAX_SPEED)
--- nicks-playground/game.py
+++ nicks-playground/game.py
@@ -2,48 +2,94 @@ import character
 import pygame
 
 
-class Game():
-    def __init__(self, screen_width, screen_height):
+FPS = 50
+BORDER = 50
+
+
+class Game:
+    def __init__(self, screen_width: int, screen_height: int):
+        """Constructor for Game class, which controls the looping of the game.
+
+        :param screen_width: Width of screen viewport
+        :param screen_height: Height of screen viewport
+        """
         self.screen_width = screen_width
         self.screen_height = screen_height
-        self.player = character.Character("Cage", 50, 30, 3)
         self.background = pygame.image.load("C:\\users\\nick\\pictures\\game_assets\\basic-stripe.png")
-        self.viewport_corner_x = (self.background.get_size()[0] // 2) - (self.screen_width // 2)
-        self.viewport_corner_y = (self.background.get_size()[1] // 2) - (self.screen_height // 2)
+        self.background_width = self.background.get_size()[0]
+        self.background_height = self.background.get_size()[1]
+        print(self.background_width, self.background_height)
+        self.viewport_corner_x = (self.background_width // 2) - (self.screen_width // 2)
+        self.viewport_corner_y = (self.background_height // 2) - (self.screen_height // 2)
+        print(self.viewport_corner_x, self.viewport_corner_y)
+        self.player = character.Character("Cage", 50, 30,
+                                          self.viewport_corner_x + self.screen_width // 2,
+                                          self.viewport_corner_y + self.screen_height // 2)
+        print(self.player.rect.top, self.player.rect.height)
 
     def begin_game_loop(self):
+        """Does the work of running the game loop.
+
+        :return: None
+        """
         pygame.init()
         clock = pygame.time.Clock()
         # Set up the drawing window
         screen = pygame.display.set_mode([self.screen_width, self.screen_height])
 
-
-
         # Run until the user asks to quit
         running = True
         while running:
-
+            self.update_assets(screen)
             # Did the user click the window close button?
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
 
-            # Fill the background with black
-            screen.fill((0, 0, 0))
-
-            screen.blit(self.player.image, self.player.rect)
-            keys = pygame.key.get_pressed()
-            self.player.update(keys, self.screen_width, self.screen_height)
-
-            # Flip the display
-            pygame.display.flip()
-            clock.tick(50)
+            clock.tick(FPS)
 
         # Done! Time to quit.
         pygame.quit()
         print("Bye bye Cage!")
 
-    def updateAssets(self):
-        print("TODO")
+    def update_assets(self, screen):
+        # Fill the background with black
+        screen.fill((0, 0, 0))
+
+        keys = pygame.key.get_pressed()
+        self.player.update(keys, self.background_width, self.background_height)
+        # print(self.player.rect.left, self.viewport_corner_x, self.screen_width)
+        # Now we try to move the background based upon player movement
+        # check if player has moved to the right past 20% from right side
+        if self.player.rect.left + self.player.rect.width > self.viewport_corner_x + round(.8 * self.screen_width):
+            # player left x + width > viewport corner left x + 80% screen width
+            self.viewport_corner_x += self.player.speed
+        # check if the player has moved to the left past 20% from the left side
+        elif self.player.rect.left < self.viewport_corner_x + round(.2 * self.screen_width):
+            # player left x < viewport corner left x + 20% screen width
+            self.viewport_corner_x -= self.player.speed
+        # ensure the viewport x is valid
+        if self.viewport_corner_x > self.background_width + 2*BORDER - self.screen_width:
+            self.viewport_corner_x = self.background_width + 2*BORDER - self.screen_width
+        if self.viewport_corner_x < 0:
+            self.viewport_corner_x = 0
+
+        # check if player has moved up past 20% from top
+        if self.player.rect.top - self.player.rect.height < self.viewport_corner_y + round(.2 * self.screen_height):
+            self.viewport_corner_y -= self.player.speed
+        # check if the player has down past 20% from bottom
+        elif self.player.rect.top > self.viewport_corner_y + round(.8 * self.screen_height):
+            self.viewport_corner_y += self.player.speed
+        # ensure the viewport y is valid
+        if self.viewport_corner_y > self.background_height + 2 * BORDER - self.screen_height:
+            self.viewport_corner_y = self.background_height + 2 * BORDER - self.screen_height
+        if self.viewport_corner_y < 0:
+            self.viewport_corner_y = 0
+
+        screen.blit(self.background, (BORDER - self.viewport_corner_x, BORDER - self.viewport_corner_y))
+        screen.blit(self.player.image, (BORDER + self.player.rect.left - self.viewport_corner_x,
+                    BORDER + self.player.rect.top - self.viewport_corner_y))
+        # Flip the display
+        pygame.display.flip()
 
 
